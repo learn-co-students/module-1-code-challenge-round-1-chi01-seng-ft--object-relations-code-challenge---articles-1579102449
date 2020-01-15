0711e049dcd29e19bf8b54bbff4798f60d405a1b
Linda <ltramos7@gmail.com>
2020-01-15 11:20:42 -0600

    Code Challenge

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,36 @@
+
+#think about attr_reader & attr_accessor
+
+# attr_accessor :magazine, :title
+# attr_reader :author
+
 class Article
 
+    # attr_reader :author #:magazine, :title
+    attr_accessor :author, :magazine, :title
+    @@all = []
+
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+    end
+
+    def title
+        @title
+    end
+
+    def self.all
+        @@all
+    end
+
+    def authors
+        self.author.name
+    end
+
+    def magazines
+        self.magazine.name # I know article_1.magazine will give me the instance of the magazine
+    end
 
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,29 @@
 class Author
   attr_accessor :name
+  #origninally attr_accessor, but to change to attr_reader to not change?
 
 
   def initialize(name)
     @name = name
-  
+
+  end
+
+  def articles
+    # want the articles written by the author(Jack London)
+    Article.all.map do |author_instances|
+      author_instances.name
+    end
+
+    # loop through articles
+    # get to names that match the articles
+    #
+    
+    
+    # select through the articles and pull out == name
+  end
+
+  def magazingess
+
   end
 
 
