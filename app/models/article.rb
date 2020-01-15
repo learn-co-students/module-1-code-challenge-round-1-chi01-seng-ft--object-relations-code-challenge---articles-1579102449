76a1006f0f52adb23e178d9127e409fccbfb90db
Cesar <cesarmota@ymail.com>
2020-01-15 11:28:07 -0600

    cesar's first code challenge

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,27 @@
 class Article
+    attr_reader :author, :magazine, :title
+    @@all = []
 
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+    end
+
+
+    def self.all
+        @@all
+    end
+
+    def author #fix!!
+        @author
+    end
+
+
+    def magazine
+        @magazine
+    end
 
 end
+
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
 
   def initialize(name)
@@ -7,5 +7,37 @@ class Author
   
   end
 
+  # def author_instances
+  #   
+  # end
+
+  def articles #fix!
+    Article.all.select do |articles|
+       articles.author == self
+    end
+  end
+
+  def magazines
+      author_articles = Article.all.select do |articles|
+      articles.author == self
+      end
+      author_articles.select do |articles|
+        articles.magazine
+      end
+
+  end
+
+  def add_article(magazine,title)
+      new_article = Article.new(self, magazine, title)
+      new_article.author = self
+  end
+
+  def topic_areas
+    Magazine.all.select do |articles|
+    articles.author == self
+    end. select do |articles|
+    articles.category
+    end
+  end
 
 end
