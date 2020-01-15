f04afb95d73cb5272305c7b711349104c84313a0
Lindsay Mecher <lindsaymecher@gmail.com>
2020-01-15 11:20:02 -0600

    First coding challenge :)

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,34 @@
 class Article
 
+    attr_accessor :author, :magazine, :title
 
+    @@all = []
+
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
+
+# #### Article
+
+# - `Article#initialize(author, magazine, title)` DONE
+#   - An article is initialized with an author as an Author object, a magazine as a Magazine object, and title as a string.
+#   - An article **cannot** change its author, magazine, or title after it is has been initialized.
+# - `Article#title` DONE
+#   - Returns the title for that given article
+# - `Article.all` DONE
+#   - Returns an array of all Article instances
+
+# #### Article
+
+# - `Article#author` instance DONE or name????
+#   - Returns the author for that given article
+# - `Article#magazine` instance DONE or name???
+#   - Returns the magazine for that given article
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author
-  attr_accessor :name
+  attr_accessor
+  attr_reader :name
 
 
   def initialize(name)
@@ -7,5 +8,65 @@ class Author
   
   end
 
+  def articles
+    Article.all.select do |article_instance|
+      article_instance.author == self
+    end
+  end
+
+  def my_article_instances
+    my_instances = Article.all.select do |article_instance|
+      article_instance.author == self
+    end
+  end
+
+  def magazines
+    #refactored this to use the above method to be called on by topic areas
+    # my_instances = Article.all.select do |article_instance|
+    #   article_instance.author == self
+    # end
+    # my_instances
+    self.my_article_instances.map do |instance|
+      instance.magazine
+    end.uniq
+  end
+
+
+  def add_article(magazine, title)
+    Article.new(self, magazine, title)
+  end
+
+  def topic_areas
+    #map over all magazines, not unique, to collect all the articles, then return unique categories
+    my_magazines = self.my_article_instances.map do |instance|
+      instance.magazine
+    end
+    my_magazines.map do |magazines|
+      magazines.category
+    end.uniq
+  end
+
 
 end
+
+# #### Author
+
+# - `Author#initialize(name)` DONE
+#   - An author is initialized with a name, as a string.
+#   - A name **cannot** be changed after it is initialized.
+# - `Author#name` DONE
+#   - Returns the name of the author
+
+#### Author
+
+# - `Author#articles` DONE
+#   - Returns an array of Article instances the author has written
+# - `Author#magazines` DONE
+#   - Returns a **unique** array of Magazine instances for which the author has contributed to
+
+#### Author
+
+# - `Author#add_article(magazine, title)` DONE
+#   - Given a magazine (as Magazine instance) and a title (as a string), creates a new Article instance and associates it with that author and that magazine.
+# - `Author#topic_areas`  DONE
+#   - Returns a **unique** array of strings with the categories of the magazines the author has contributed to
