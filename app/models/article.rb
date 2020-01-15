a20dc9082ff06a4bf6e4b5125abc89a95921368d
m-gizzi <matthewgizzi@verizon.net>
2020-01-15 10:53:53 -0600

    Code Challenge completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,24 @@
 class Article
 
+    attr_reader :author, :magazine, :title
+
+    @@all = []
+
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.count_by_author_by_magazine(author, magazine)
+        Article.all.count do |article|
+            article.author == author && article.magazine == magazine
+        end
+    end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,12 +6,15 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
+stephen_king = Author.new("Stephen King")
+neil_gaiman = Author.new("Neil Gaiman")
 
+wired = Magazine.new("Wired", "science")
+new_yorker = Magazine.new("New Yorker", "stories")
 
-
-
-
-
+on_writing = Article.new(stephen_king, new_yorker, "On Writing")
+american_gods = Article.new(neil_gaiman, wired, "American Gods")
+carrie = Article.new(stephen_king, new_yorker, "Carrie")
 
 
 
