a1a2570577cf534c30f9d79a1810c08ae12fd83b
github username <github email address>
2020-01-15 11:17:56 -0600

    code challenge solution

--- solution.rb
+++ solution.rb
@@ -1,1 +1,90 @@
 # Please copy/paste all three classes into this file to submit your solution!
+class Magazine
+    attr_accessor :name, :category
+  
+    @@all = []
+  
+  
+    def initialize(name, category)
+      @name = name
+      @category = category
+      @@all << self
+  
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    def contributors
+      articles = Article.all.select{|article| article.magazine == self}
+      articles.map{|article| article.author}
+    end
+  
+    def self.find_by_name(name)
+      self.all.find{|magazine| magazine.name == name}
+    end
+  
+    def article_titles
+      articles = Article.all.select{|article| article.magazine == self}
+      articles.map{|article| article.title}
+    end
+  
+    def contributing_authors
+      contributions = Hash.new(0)
+      contributors.each{|author| contributions[author] += 1}
+      contributions.select!{|k,v| v > 2}
+      contributions.map{|k,v| k}
+    end
+  
+  
+  
+  end
+
+  class Author
+    attr_reader :name
+  
+  
+    def initialize(name)
+      @name = name
+    
+    end
+  
+    def articles
+      Article.all.select{|article| article.author == self}
+    end
+  
+    def magazines
+      self.articles.map{|article| article.magazine}.uniq
+    end
+  
+    def add_article(magazine, title)
+      Article.new(self, magazine, title)
+    end
+  
+    def topic_areas
+      self.magazines.map{|magazine| magazine.category}.uniq
+    end
+    
+  
+  
+  
+  end
+
+  class Article
+    attr_reader :author, :magazine, :title 
+    @@all = []
+
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+end
