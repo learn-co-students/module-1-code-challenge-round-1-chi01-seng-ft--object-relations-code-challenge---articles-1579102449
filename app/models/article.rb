8b454f76e651205947e8966328b48caf7951eadb
learn <github email address>
2020-01-15 09:18:32 -0800

    Couldn't figure out last method to return the amount of times someone appeared

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,30 @@
 class Article
 
+    attr_reader :author, :magazine, :title, :all
+
+    @@all = []
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+    end
+
+        def title
+            @title
+        end
+
+
+        def author 
+            @author
+        end
+
+        def magazine
+            @magazine
+        end
+
+        def self.all
+            @@all
+        end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,6 +7,27 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+keenan = Author.new("Keenan")
+odin = Magazine.new("Odin", "Business")
+blazing = Article.new(keenan, odin, "Blazing")
+
+jessie = Author.new("Jessie")
+roses = Magazine.new("Roses", "News")
+rose_water = Article.new(jessie, roses, "RoseWater")
+
+linda = Author.new("Linda")
+monsters = Magazine.new("Monsters", "History")
+women = Article.new(linda, odin, "Women")
+
+
+legends = Article.new(keenan, monsters, "History")
+
+mythical_legends = Article.new(keenan, odin, "Futures")
+
+
+
+
+
 
 
 
