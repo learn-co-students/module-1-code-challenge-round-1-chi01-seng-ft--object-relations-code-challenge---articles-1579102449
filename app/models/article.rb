5e49563dd27965aa5024f942916f88698121b234
Gonzalez32 <3232rose@gmail.com>
2020-01-15 11:29:05 -0600

    done

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,30 @@
 class Article
 
+    attr_reader :author, :magazine, :title
+    @@all = []
+
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author
+        @author << author
+        author.article = self 
+    end
+
+    def magazine
+        @magazine << magazine
+        magazine.article = self
+    end
+    
+    
+
 
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,18 @@
 class Author
-  attr_accessor :name
-
+  attr_reader :name
+  
 
   def initialize(name)
     @name = name
-  
+    
   end
 
+  def add_article(magazine, title)
+    #finding the right santax for this method. 
+  end
 
+  def topic_areas
+    #same here finding the right santax for this method as well 
+  end
+  
 end
