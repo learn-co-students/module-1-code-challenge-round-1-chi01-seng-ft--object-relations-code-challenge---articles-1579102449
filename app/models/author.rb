641bdad1fe3c3cbcd51bfd1ab5d1b33377aceb18
github username <github email address>
2020-01-15 11:18:01 -0600

    Christine Doherty Code Challenge 1 Solution

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,37 @@
 class Author
-  attr_accessor :name
+  attr_reader :name, :magazine, :title
 
+  @@all = []
 
   def initialize(name)
     @name = name
   
+    @@all << self
+  end
+
+  def add_article(magazine, title)
+    @magazine = magazine 
+    @title = title
+
+    Article.new(self, magazine, title)
+  end
+
+  def articles
+    Article.all.select {|a| a.author == self}
+  end
+
+  def magazines
+    x = articles.map{|a| a.magazine}.uniq
+    # x.map {|m| m.name}  ##Re turns only name of mags in array  -- easier to see 
+  end
+
+  def topic_areas
+    mags = articles.map {|a| a.magazine}.uniq
+    mags.map {|m| m.category}
+  end
+
+  def self.all
+    @@all
   end
 
 
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,11 +1,46 @@
 class Magazine
   attr_accessor :name, :category
 
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
 
+    @@all << self
+  end
+
+  def articles 
+    Article.all.select {|a| a.magazine == self}
+  end
+
+  def contributors 
+    authors = articles.map {|mag| mag.author}.uniq
+    # authors.map {|a| a.name}  ## Returns only names in array
+  end 
+
+  def article_titles
+   articles.map {|a| a.title}
+  end
+
+  def contributing_authors
+    authors = articles.map {|mag| mag.author}
+    contr = authors.find_all{ |a| authors.count(a) > 2}## Found on 
+    #https://stackoverflow.com/questions/8921999/ruby-how-to-find-and-return-a-duplicate-value-in-array/31354262
+    contr.map {|c| c.name}.uniq ## Maybe not the best syntax?
+
+
+    # authors.any? do |y| x = authors.count(y) > 2 
+    # end
+    # authors.count{|a| a.name}
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|m| m.name == name}
+  end
+
+  def self.all 
+    @@all
   end
 
 
